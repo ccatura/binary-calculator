0c2ee812e8ccb48b2234d12bd7724aea109909ac
ccatura <charlie@charliecatura.com>
2022-01-17 20:45:15 -0500

    Did a bunch of stuff

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-This will definitely NOT be empty!
+// All for buttons
+let displayValue = '0';
+document.getElementById('readout').innerHTML = parseInt(displayValue);
+
+btns = document.getElementsByClassName('button');
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click',redirect,false);
+}
+
+function redirect(){
+  if (displayValue.length < 12) {
+    displayValue = parseInt(displayValue) + this.innerHTML;
+    // displayValue = parseInt(displayValue);
+    document.getElementById('readout').innerHTML = parseInt(displayValue);
+    console.log(displayValue);
+  }
+}
+
